b2ebc9a048cb367b60da056205f4e26861114323
yousra212 <bakkaliyousra8@gmail.com>
2023-01-04 21:26:42 +0100

    added the carousel on FAQ

--- rules/script.js
+++ rules/script.js
@@ -1,4 +1,4 @@
-
+                       //  Javascript for the message on the terms of use's button
 
 // Get a reference to the accept and decline buttons
 const acceptButton = document.getElementById('accept-button');
@@ -24,4 +24,48 @@ function init() {
 
 // Call the init function when the DOM is ready
 document.addEventListener('DOMContentLoaded', init);
+
+
+
+
+                        // Javascript for the carousel
+                        
+document.querySelectorAll(".carousel").forEach((carousel) => {
+  const items = carousel.querySelectorAll(".carousel__item");
+  const buttonsHtml = Array.from(items, () => {
+    return `<span class="carousel__button"></span>`;
+  });
+
+  carousel.insertAdjacentHTML(
+    "beforeend",
+    `
+		<div class="carousel__nav">
+			${buttonsHtml.join("")}
+		</div>
+	`
+  );
+
+  const buttons = carousel.querySelectorAll(".carousel__button");
+
+  buttons.forEach((button, i) => {
+    button.addEventListener("click", () => {
+      // un-select all the items
+      items.forEach((item) =>
+        item.classList.remove("carousel__item--selected")
+      );
+      buttons.forEach((button) =>
+        button.classList.remove("carousel__button--selected")
+      );
+
+      items[i].classList.add("carousel__item--selected");
+      button.classList.add("carousel__button--selected");
+    });
+  });
+
+  // Select the first item on page load
+  items[0].classList.add("carousel__item--selected");
+  buttons[0].classList.add("carousel__button--selected");
+});
+
+
 
